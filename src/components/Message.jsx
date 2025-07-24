ffa0e5699e734658cb8b407081ff37922ff777ee
Israel <israrelo@gmail.com>
2025-07-24 15:41:16 -0500

    Prepare deploy and format files

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,6 +1,6 @@
-import React, { useRef, useState} from 'react';
-import emailjs from '@emailjs/browser';
-import './Message.css';
+import React, { useRef, useState } from "react";
+import emailjs from "@emailjs/browser";
+import "./Message.css";
 
 export default function Message() {
   const form = useRef();
@@ -10,38 +10,58 @@ export default function Message() {
   const sendEmail = (e) => {
     e.preventDefault();
 
-    emailjs.sendForm(
-  import.meta.env.VITE_EMAILJS_SERVICE_ID,
-  import.meta.env.VITE_EMAILJS_TEMPLATE_ID,
-  form.current,
-  import.meta.env.VITE_EMAILJS_PUBLIC_KEY
-    ).then(() => {
-      setMessageStatus("Message sent successfully!");
-      setShowPopup(true);
-      setTimeout(() => setShowPopup(false), 3000); 
-    }, () => {
-      setMessageStatus("Failed to send message.");
-      setShowPopup(true);
-      setTimeout(() => setShowPopup(false), 3000);
-    });
+    emailjs
+      .sendForm(
+        import.meta.env.VITE_EMAILJS_SERVICE_ID,
+        import.meta.env.VITE_EMAILJS_TEMPLATE_ID,
+        form.current,
+        import.meta.env.VITE_EMAILJS_PUBLIC_KEY
+      )
+      .then(
+        () => {
+          setMessageStatus("Message sent successfully!");
+          setShowPopup(true);
+          setTimeout(() => setShowPopup(false), 3000);
+        },
+        () => {
+          setMessageStatus("Failed to send message.");
+          setShowPopup(true);
+          setTimeout(() => setShowPopup(false), 3000);
+        }
+      );
 
     e.target.reset();
   };
 
   return (
     <form ref={form} onSubmit={sendEmail} className="eform">
-    <div className='contact'>
-      <input type="text" name="user_name" placeholder="Your Name" required title="Please enter a valid email address"/>
-      <input type="email" name="user_email" placeholder="Your Email" required/>
-   </div> 
-      <textarea name="message" placeholder="Your Message" required/>
+      <div className="contact">
+        <input
+          type="text"
+          name="user_name"
+          placeholder="Your Name"
+          required
+          title="Please enter a valid email address"
+        />
+        <input
+          type="email"
+          name="user_email"
+          placeholder="Your Email"
+          required
+        />
+      </div>
+      <textarea name="message" placeholder="Your Message" required />
       <button type="submit">Send</button>
 
-        {showPopup && (
-        <div className={`floating-popup ${messageStatus.includes("Failed") ? "error" : "success"}`}>
-            {messageStatus}
+      {showPopup && (
+        <div
+          className={`floating-popup ${
+            messageStatus.includes("Failed") ? "error" : "success"
+          }`}
+        >
+          {messageStatus}
         </div>
-        )}
+      )}
     </form>
   );
 }
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,22 +1,20 @@
-import React from 'react';
-import './NavBar.css';
+import React from "react";
+import "./NavBar.css";
 
 export default function NavBar() {
   return (
     <>
-        <nav>
-            <div className='left'>
-                <a href="#about">Israel Reveles</a>
-            </div>
-            <div className='right'>
-                <a href="#about">About</a>
-                <a href="#project">Projects</a>
-                <a href="#skills">Skills</a>
-                <a href="#message">Leave a message</a>
-            </div>
-        </nav>
+      <nav>
+        <div className="left">
+          <a href="#about">Israel Reveles</a>
+        </div>
+        <div className="right">
+          <a href="#about">About</a>
+          <a href="#project">Projects</a>
+          <a href="#skills">Skills</a>
+          <a href="#message">Leave a message</a>
+        </div>
+      </nav>
     </>
   );
 }
-
-
